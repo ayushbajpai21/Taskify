71215054ce71119c20818f5172bfa4f4cf02d293
ayushbajpai21 <167740600+ayushbajpai21@users.noreply.github.com>
2026-01-21 23:03:18 +0530

    Refactor axios instance to use environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // JWT cookie allow
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true
 });
 
-export default api;
+export default API;
